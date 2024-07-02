e6b4515bb980b1b739908ae9eec1761626b61f5f
niluse <niluse02@gmail.com>
2024-07-02 11:19:31 +0300

    feat: check for columns of three if exists make it an empty string

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 const App = () => {
   const [board, setBoard] = useState([]);
 
+  // const checkForColumnOfThree = () => {
+  //   for (let i = 0; i <= 39; i++) {
+  //     const columnOfCandies = [];
+  //     for (let j = 1; j <= 8; j++) {
+  //       const currentCandyIndex = (j + j * i) - 1;
+  //       const currentCandy = board[currentCandyIndex];
+  //       columnOfCandies.push(currentCandy);
+  //     }
+  //     const targetColumn = columnOfCandies[0];
+  //     if (columnOfCandies.length >= 3 && columnOfCandies.every((candy) => candy === targetColumn))
+  //       {
+  //         return true;
+  //       }
+  //       }
+  //       return false;
+  // }
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = board[i];
+      if (columnOfThree.every((index) => board[index] === decidedColor)) {
+        columnOfThree.forEach((index) => (board[index] = ""));
+      }
+    }
+  };
+
   const createBoard = () => {
     const newBoard = [];
     for (let i = 0; i < width * width; i++) {
@@ -20,7 +46,14 @@ const App = () => {
   useEffect(() => {
     createBoard();
   }, []);
-  console.log(board);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setBoard([...board]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkForColumnOfThree, board]);
 
   return (
     <div className="App">
