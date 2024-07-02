560dd909d7ee27a977e39211ac2f32272e0d07d3
niluse <niluse02@gmail.com>
2024-07-02 10:54:13 +0300

    feat: game board

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const width = 8;
 const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const [board, setBoard] = useState([]);
+
+  const createBoard = () => {
+    const newBoard = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomNumber = Math.floor(Math.random() * candyColors.length);
+      const randomColor = candyColors[randomNumber];
+      newBoard.push(randomColor);
+    }
+    setBoard(newBoard);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+  console.log(board);
+
+  return (
+    <div className="App">
+      <div className="board">
+        {board.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
